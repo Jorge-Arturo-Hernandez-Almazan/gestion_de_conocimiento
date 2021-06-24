f4c44bc1d6966a2da85a64cb49d1fcbcc94d7248
Froylán M. Wbario Martínez <1530006@upv.edu.mx>
2021-06-24 11:26:20 -0500

    Limpieza del proyecto

--- app/Controllers/Http/RelacionNodoAlumnoController.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-class RelacionNodoAlumnoController {
-}
-
-module.exports = RelacionNodoAlumnoController
--- app/Models/Tema.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
-
-class Tema extends Model {
-}
-
-module.exports = Tema
